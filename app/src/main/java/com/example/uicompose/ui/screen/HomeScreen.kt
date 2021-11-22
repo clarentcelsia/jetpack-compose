86e0dd23e16f67f675d3597d09ee8e64d02abe01
clarentclaire <xozhengxiao@gmail.com>
2021-11-22 21:09:12 +0700

    State

--- app/src/main/java/com/example/uicompose/ui/screen/HomeScreen.kt
+++ app/src/main/java/com/example/uicompose/ui/screen/HomeScreen.kt
@@ -1,98 +1,69 @@
 package com.example.uicompose.ui.screen
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.example.uicompose.R
-import com.example.uicompose.ui.theme.Purple200
+import kotlin.random.Random
 
+// https://developer.android.com/jetpack/compose/state
 @Composable
-fun HomeScreen(text: String) {
-    val painter = painterResource(id = R.drawable.imgg)
-    val contentDescription = "image"
-    Box(
-        modifier = Modifier
-            .fillMaxWidth(0.5f)
-            .padding(12.dp)
-    ) {
-        ImageCard(
-            painter = painter,
-            text = text,
-            contentDescription = contentDescription
-        )
+fun HomeScreen() {
+    Column {
+        ClickBox()
+        TextFIeld()
     }
-
 }
 
 @Composable
-fun ImageCard(
-    painter: Painter,
-    text: String,
-    contentDescription: String,
+fun TextFIeld(modifier: Modifier = Modifier) {
+    //Name yang akan diobserve setiap saat.
+    var name by remember { mutableStateOf("") }
+
+    OutlinedTextField(
+        value = name,
+        onValueChange = { name = it },
+        modifier = Modifier.fillMaxWidth()
+    )
+
+    Text(text = name, color = Color.Black)
+}
+
+@Composable
+fun ClickBox(
     modifier: Modifier = Modifier
 ) {
-    Card(
-        modifier = Modifier.fillMaxWidth(),
-        shape = RoundedCornerShape(5.dp),
-        elevation = 8.dp
-    ) {
-        Box(modifier = Modifier.height(150.dp)) {
-            Image(
-                painter = painter,
-                contentDescription = contentDescription,
-                contentScale = ContentScale.Crop
-            )
-
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(
-                        Brush.verticalGradient(
-                            listOf(
-                                Color.Transparent,
-                                Color.Black
-                            ),
-                            startY = (0.2*150.dp.value).toFloat()
-                        )
-                    )
-            )
-
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(12.dp),
-                contentAlignment = Alignment.BottomStart
-            ) {
-                Text(
-                    text = text,
-                    style = TextStyle(
-                        color = Purple200,
-                        fontSize = 16.sp
-                    )
+    //Berubah pada saat tertentu
+    val color = remember { mutableStateOf(Color.Green) }
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(50.dp)
+            .background(color.value)
+            .clickable {
+                //Change color every time box is clicked
+                color.value = Color(
+                    Random.nextFloat(),
+                    Random.nextFloat(),
+                    Random.nextFloat()
                 )
-            }
 
-        }
-    }
+            }
+    )
 }
 
 @Preview
 @Composable
 fun DefaultPreview() {
-    HomeScreen(text = "adios")
+    HomeScreen()
 }
