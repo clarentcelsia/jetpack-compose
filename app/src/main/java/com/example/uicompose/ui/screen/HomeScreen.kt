2a838e9ddca8e40e24b0596157ec0a3035ed746e
clarentclaire <xozhengxiao@gmail.com>
2021-11-22 14:13:48 +0700

    Image Card

--- app/src/main/java/com/example/uicompose/ui/screen/HomeScreen.kt
+++ app/src/main/java/com/example/uicompose/ui/screen/HomeScreen.kt
@@ -1,73 +1,98 @@
 package com.example.uicompose.ui.screen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.uicompose.R
 import com.example.uicompose.ui.theme.Purple200
 
 @Composable
 fun HomeScreen(text: String) {
-    Row {
-        Text(
+    val painter = painterResource(id = R.drawable.imgg)
+    val contentDescription = "image"
+    Box(
+        modifier = Modifier
+            .fillMaxWidth(0.5f)
+            .padding(12.dp)
+    ) {
+        ImageCard(
+            painter = painter,
             text = text,
-            color = Purple200,
-            fontSize = 20.sp,
-            fontFamily = FontFamily.SansSerif,
-            fontStyle = FontStyle.Italic,
-            textAlign = TextAlign.Center,
-            modifier = Modifier
-                .background(Color.Black)
-                .weight(3f)
-                .border(1.5.dp, Color.White, RectangleShape)
-                .padding(10.dp)
+            contentDescription = contentDescription
         )
+    }
 
-        Text(
-            text = text,
-            color = Purple200,
-            fontSize = 20.sp,
-            fontFamily = FontFamily.SansSerif,
-            fontStyle = FontStyle.Italic,
-            textAlign = TextAlign.Left,
-            modifier = Modifier
-                .background(Color.DarkGray)
-                .weight(2f)
-                .border(1.5.dp, Color.White, RectangleShape)
-                .padding(start = 10.dp)
-        )
+}
 
-        Text(
-            text = text,
-            color = Purple200,
-            fontSize = 20.sp,
-            fontFamily = FontFamily.SansSerif,
-            fontStyle = FontStyle.Italic,
-            textAlign = TextAlign.Right,
-            modifier = Modifier
-                .background(Color.White)
-                .weight(1f)
-                .border(1.5.dp, Color.Black, RectangleShape)
-                .padding(start = 10.dp)
-        )
+@Composable
+fun ImageCard(
+    painter: Painter,
+    text: String,
+    contentDescription: String,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = Modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(5.dp),
+        elevation = 8.dp
+    ) {
+        Box(modifier = Modifier.height(150.dp)) {
+            Image(
+                painter = painter,
+                contentDescription = contentDescription,
+                contentScale = ContentScale.Crop
+            )
+
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        Brush.verticalGradient(
+                            listOf(
+                                Color.Transparent,
+                                Color.Black
+                            ),
+                            startY = (0.2*150.dp.value).toFloat()
+                        )
+                    )
+            )
+
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(12.dp),
+                contentAlignment = Alignment.BottomStart
+            ) {
+                Text(
+                    text = text,
+                    style = TextStyle(
+                        color = Purple200,
+                        fontSize = 16.sp
+                    )
+                )
+            }
+
+        }
     }
 }
 
 @Preview
 @Composable
 fun DefaultPreview() {
-    HomeScreen(text = "Adios")
+    HomeScreen(text = "adios")
 }
