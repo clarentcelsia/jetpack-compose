39251ad9a55e969b0c44804fbf0758e3665374c0
clarentclaire <xozhengxiao@gmail.com>
2021-11-23 19:46:56 +0700

    Navigation Drawer

--- app/src/main/java/com/example/uicompose/ui/screen/HomeScreen.kt
+++ app/src/main/java/com/example/uicompose/ui/screen/HomeScreen.kt
@@ -1,114 +1,133 @@
 package com.example.uicompose.ui.screen
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Face
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.uicompose.ui.theme.Purple200
 import kotlinx.coroutines.launch
 
-// https://developer.android.com/jetpack/compose/state
+@ExperimentalMaterialApi
 @Composable
 fun HomeScreen() {
-    Scaffolds()
+    BottomDrawers()
 }
 
 @Composable
-fun Buttons() {
-    Column {
-        Button(
-            onClick = { },
-            contentPadding = PaddingValues(
-                top = 8.dp,
-                bottom = 8.dp,
-                start = 12.dp,
-                end = 12.dp
-            ),
-            enabled = true,
-            colors = ButtonDefaults.buttonColors(Purple200)
-        ) {
-            //Inner content including an icon and a text label
-            Icon(
-                Icons.Filled.Face,
-                "myface",
-                modifier = Modifier.size(ButtonDefaults.IconSize)
-            )
-            Spacer(modifier = Modifier.size(ButtonDefaults.IconSpacing))
-            Text(text = "Click Me!")
-        }
-
-        Spacer(modifier = Modifier.height(10.dp))
-
-        ExtendedFloatingActionButton(
-            text = {
-                Text(text = "Save me!")
-            },
-            onClick = { },
-            icon = {
-                Icon(
-                    Icons.Filled.Notifications,
-                    contentDescription = "save me"
-                )
-            }
-        )
-    }
-
-}
-
-//https://developer.android.com/jetpack/compose/layouts/material
-// Layout
-@Composable
-fun Scaffolds() {
+fun SideDrawer() {
     val scaffoldState = rememberScaffoldState()
     val scope = rememberCoroutineScope()
     Scaffold(
+        //init
         scaffoldState = scaffoldState,
-        topBar = {
-            TopAppBar(
-                backgroundColor = Purple200,
-                elevation = 5.dp
-            ) {
-                //Row content
-            }
+        drawerBackgroundColor = Purple200,
+        drawerContent = {
+            Text(text = "Title")
+            Divider()
+            Text(text = "Hello")
+            //Drawer Items
         },
-        bottomBar = {
-            BottomAppBar(backgroundColor = Purple200, elevation = 5.dp) {
-                //content
-            }
-        },
+        drawerGesturesEnabled = true,
         floatingActionButton = {
-            FloatingActionButton(onClick = {
-                scope.launch {
-                    val result = scaffoldState.snackbarHostState
-                        .showSnackbar("Show snackbar")
-
-                    when(result){
-                        SnackbarResult.ActionPerformed ->{}
-                        SnackbarResult.Dismissed->{}
+            ExtendedFloatingActionButton(text = { Text(text = "Open or Close Drawer") },
+                onClick = {
+                    //run
+                    scope.launch {
+                        scaffoldState.drawerState.apply {
+                            if (this.isClosed) open()
+                            else close()
+                        }
                     }
-                }
-            }, backgroundColor = Purple200) {
-                Text(text = "Button")
-            }
+                })
+        }
+    ) {
+        //Screen Content
+    }
+
+    // Implementing navigation drawer without scaffold
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    ModalDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            Text(text = "This is an example of the usage of Modal Drawer")
+            Divider(thickness = 2.dp)
+            Text(text = "Modal Drawer")
         },
-        isFloatingActionButtonDocked = true
+        gesturesEnabled = true,
+        drawerBackgroundColor = Purple200,
+        scrimColor = Color.Red,
+        drawerElevation = 5.dp
     ) {
         //screen content
-        Column {
-            Text(text = "Hello")
-            Spacer(modifier = Modifier.height(10.dp))
-            Buttons()
-        }
-
+        Text(text = "Content")
     }
 }
 
+@ExperimentalMaterialApi
+@Composable
+fun BottomDrawers() {
+    val (gesturesEnabled, toggleGesturesEnabled) = remember { mutableStateOf(true) }
+    val drawerState = rememberBottomDrawerState(initialValue = BottomDrawerValue.Closed)
+    val scope = rememberCoroutineScope()
+    BottomDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            Button(
+                onClick = {
+                    scope.launch {
+                        if (drawerState.isClosed) drawerState.open()
+                        else drawerState.close()
+                    }
+                },
+                content = { Text(text = "Click") }
+            )
+            LazyColumn {
+                items(25) {
+                    ListItem(
+                        text = { Text("Item $it") },
+                        icon = {
+                            Icon(
+                                Icons.Default.Favorite,
+                                contentDescription = "description"
+                            )
+                        }
+                    )
+                }
+            }
+        }
+    )
+    {
+        //screen content
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            val openText = if (gesturesEnabled) " Pull up " else "Click the button!"
+            Text(text = if (drawerState.isClosed) openText else "Drag down")
+            Spacer(Modifier.height(20.dp))
+            Button(onClick = { scope.launch { drawerState.open() } }) {
+                Text("Click to open")
+            }
+        }
+    }
+
+}
+
+@ExperimentalMaterialApi
 @Preview
 @Composable
 fun DefaultPreview() {
