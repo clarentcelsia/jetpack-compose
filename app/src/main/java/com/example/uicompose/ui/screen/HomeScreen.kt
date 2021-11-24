f3939f275c6bb628e39af6eeda6ebbba3f26ff12
clarentclaire <xozhengxiao@gmail.com>
2021-11-24 11:53:45 +0700

    Backdrop Scaffold

--- app/src/main/java/com/example/uicompose/ui/screen/HomeScreen.kt
+++ app/src/main/java/com/example/uicompose/ui/screen/HomeScreen.kt
@@ -1,9 +1,11 @@
 package com.example.uicompose.ui.screen
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.*
@@ -15,88 +17,79 @@ import androidx.compose.ui.unit.dp
 import com.example.uicompose.ui.theme.Purple200
 import kotlinx.coroutines.launch
 
+//https://developer.android.com/reference/kotlin/androidx/compose/material/package-summary#backdropscaffold
 @ExperimentalMaterialApi
 @Composable
 fun HomeScreen() {
-    BottomSheet()
-}
-
-@Composable
-fun SideDrawer() {
-    val scaffoldState = rememberScaffoldState()
-    val scope = rememberCoroutineScope()
-    Scaffold(
-        //init
-        scaffoldState = scaffoldState,
-        drawerBackgroundColor = Purple200,
-        drawerContent = {
-            Text(text = "Title")
-            Divider()
-            Text(text = "Hello")
-            //Drawer Items
-        },
-        drawerGesturesEnabled = true,
-        floatingActionButton = {
-            ExtendedFloatingActionButton(text = { Text(text = "Open or Close Drawer") },
-                onClick = {
-                    //run
-                    scope.launch {
-                        scaffoldState.drawerState.apply {
-                            if (this.isClosed) open()
-                            else close()
-                        }
-                    }
-                })
-        }
-    ) {
-        //Screen Content
-    }
-
-    // Implementing navigation drawer without scaffold
-    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
-    ModalDrawer(
-        drawerState = drawerState,
-        drawerContent = {
-            Text(text = "This is an example of the usage of Modal Drawer")
-            Divider(thickness = 2.dp)
-            Text(text = "Modal Drawer")
-        },
-        gesturesEnabled = true,
-        drawerBackgroundColor = Purple200,
-        scrimColor = Color.Red,
-        drawerElevation = 5.dp
-    ) {
-        //screen content
-        Text(text = "Content")
-    }
+    BackdropScaffolds()
 }
 
 @ExperimentalMaterialApi
 @Composable
-fun BottomDrawers() {
-    val (gesturesEnabled, toggleGesturesEnabled) = remember { mutableStateOf(true) }
-    val drawerState = rememberBottomDrawerState(initialValue = BottomDrawerValue.Closed)
+fun BackdropScaffolds(){
     val scope = rememberCoroutineScope()
-    BottomDrawer(
-        drawerState = drawerState,
-        drawerContent = {
-            Button(
-                onClick = {
-                    scope.launch {
-                        if (drawerState.isClosed) drawerState.open()
-                        else drawerState.close()
+    val selection = remember { mutableStateOf(1) }
+    val scaffoldState = rememberBackdropScaffoldState(BackdropValue.Concealed)
+    LaunchedEffect(scaffoldState) {
+        scaffoldState.reveal() // back layer in reveal state
+    }
+    BackdropScaffold(
+        scaffoldState = scaffoldState,
+        appBar = {
+            TopAppBar(
+                title = { Text("Backdrop scaffold") },
+                navigationIcon = {
+                    if (scaffoldState.isConcealed) {
+                        IconButton(onClick = { scope.launch { scaffoldState.reveal() } }) {
+                            Icon(Icons.Default.Menu, contentDescription = "Localized description")
+                        }
+                    } else {
+                        IconButton(onClick = { scope.launch { scaffoldState.conceal() } }) {
+                            Icon(Icons.Default.Close, contentDescription = "Localized description")
+                        }
                     }
                 },
-                content = { Text(text = "Click") }
+                actions = {
+                    var clickCount by remember { mutableStateOf(0) }
+                    IconButton(
+                        onClick = {
+                            // show snackbar as a suspend function
+                            scope.launch {
+                                scaffoldState.snackbarHostState
+                                    .showSnackbar("Snackbar #${++clickCount}")
+                            }
+                        }
+                    ) {
+                        Icon(Icons.Default.Favorite, contentDescription = "Localized description")
+                    }
+                },
+                elevation = 0.dp,
+                backgroundColor = Color.Transparent
             )
+        },
+        backLayerContent = {
             LazyColumn {
-                items(25) {
+                items(if (selection.value >= 3) 3 else 5) {
+                    ListItem(
+                        Modifier.clickable {
+                            selection.value = it
+                            scope.launch { scaffoldState.conceal() }
+                        },
+                        text = { Text("Select $it") }
+                    )
+                }
+            }
+        },
+        frontLayerContent = {
+            Text("Selection: ${selection.value}")
+            LazyColumn {
+                items(50) {
                     ListItem(
                         text = { Text("Item $it") },
                         icon = {
                             Icon(
                                 Icons.Default.Favorite,
-                                contentDescription = "description"
+                                contentDescription = "Localized description"
                             )
                         }
                     )
@@ -104,96 +97,6 @@ fun BottomDrawers() {
             }
         }
     )
-    {
-        //screen content
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(16.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            val openText = if (gesturesEnabled) " Pull up " else "Click the button!"
-            Text(text = if (drawerState.isClosed) openText else "Drag down")
-            Spacer(Modifier.height(20.dp))
-            Button(onClick = { scope.launch { drawerState.open() } }) {
-                Text("Click to open")
-            }
-        }
-    }
-
-}
-
-@ExperimentalMaterialApi
-@Composable
-fun BottomSheet() {
-    val scope = rememberCoroutineScope()
-    val state = rememberBottomSheetScaffoldState()
-    BottomSheetScaffold(
-        scaffoldState = state,
-        backgroundColor = Purple200,
-        sheetElevation = 8.dp,
-        topBar = {
-            TopAppBar(
-                title = { Text("Bottom sheet scaffold") },
-                navigationIcon = {
-                    IconButton(onClick = { scope.launch { state.drawerState.open() } }) {
-                        Icon(Icons.Default.Menu, contentDescription = "Localized description")
-                    }
-                }
-            )
-        },
-        floatingActionButton = {
-            var clickCount by remember { mutableStateOf(0) }
-            FloatingActionButton(
-                onClick = {
-                    // show snackbar as a suspend function
-                    scope.launch {
-                        state.snackbarHostState.showSnackbar("Snackbar #${++clickCount}")
-                    }
-                }
-            ) {
-                Icon(Icons.Default.Favorite, contentDescription = "Localized description")
-            }
-        },
-        floatingActionButtonPosition = FabPosition.End,
-        sheetPeekHeight = 125.dp,
-        drawerContent = {
-            Column(
-                Modifier
-                    .fillMaxWidth()
-                    .padding(16.dp),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Text("Drawer content")
-                Spacer(Modifier.height(20.dp))
-                Button(onClick = { scope.launch { state.drawerState.close() } }) {
-                    Text("Click to close drawer")
-                }
-            }
-        },
-        sheetContent = {
-            Box(
-                Modifier
-                    .fillMaxWidth()
-                    .height(50.dp),
-                contentAlignment = Alignment.Center
-            ) {
-                Text(text = "Swipe up")
-            }
-            Column {
-                Button(onClick = {
-                    scope.launch {
-                        if (state.bottomSheetState.isCollapsed) state.bottomSheetState.expand()
-                        else state.bottomSheetState.collapse()
-                    }})
-                {
-                    Text(text = "Click")
-                }
-                Text(text = "Bottom Sheet Content")
-            }
-        }) {
-        //screen content
-    }
 }
 
 @ExperimentalMaterialApi
